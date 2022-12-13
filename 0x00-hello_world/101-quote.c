12644e0db22a407f31baf2ffd466f105d7599056
Olubusolami <olubusolao3@gmail.com>
2022-12-12 16:03:54 -0800

    C program that prints something followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Print to standard error
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			59,
+			1, stderr);
+	return (1);
+}
