38e6933d7ddf1b234175253a43dfce4d4264a162
Olubusolami <olubusolao3@gmail.com>
2022-12-12 16:48:50 -0800

    a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - print all alphabets function.
+*
+*Return: (success)
+*/
+int main(void)
+{
+	char n = '0';
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
